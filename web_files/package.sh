93886b5681fef79f6efc0aea468ab118bf0dba6c
smart <zhangdreamer@126.com>
2022-03-10 15:50:51 +0800

    update docker

---
+++ web_files/package.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#eg:
+# ./package_test.sh latest
+# ./package_test.sh
+# sudo docker login registry.cn-shanghai.aliyuncs.com
+package_version=""
+
+if [ -n "$1" ]
+then
+  package_version=$1
+else
+  package_version="0.0.1"
+fi
+
+echo "package $package_version .."
+docker build -t registry.cn-shanghai.aliyuncs.com/jrdn/nginx-web-files:0.0.1 .
+#docker push registry.cn-shanghai.aliyuncs.com/jrdn/nginx-web-files:0.0.1
